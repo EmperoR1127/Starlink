30f9ac563b19b3d29fb5e32361ea77594042952a
EmperoR <lvoldemort1127@hotmail.com>
2021-03-21 19:01:24 +0800

    add skeleton components

---
+++ src/components/SatSetting.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Form, Button, InputNumber} from 'antd';
+
+class SatSetting extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default SatSetting;
